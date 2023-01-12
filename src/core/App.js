f2964a38dc3a92b234c105f279e9d2730ea6b5e0
GrzegorzJendernal <g.jendernal@gmail.com>
2023-01-12 14:44:24 +0100

    another preparations

---
+++ src/core/App.js
@@ -0,0 +1,12 @@
+import Header from "../common/Header";
+import Wrapper from "../common/Wrapper";
+
+function App() {
+  return (
+    <Wrapper>
+      <Header />
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ src/core/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    color: {
+        white: "#FFFFFF",
+        mercury: "#E5E5E5",
+        slateGray: "#6E7E91",
+        scienceBlue: "#0366D6",
+        mineShaft: "#363636",
+        blackText: "#252525",
+
+    }
+};
