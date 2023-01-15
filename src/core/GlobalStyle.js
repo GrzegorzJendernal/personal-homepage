da30166097c0e73c3526e02ceb5c3aa94c5ef130
GrzegorzJendernal <g.jendernal@gmail.com>
2023-01-15 22:14:05 +0100

    changed background color

--- src/core/GlobalStyle.js
+++ src/core/GlobalStyle.js
@@ -13,7 +13,7 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: 'Inter', sans-serif;
-    background-color: ${({ theme }) => theme.colors.mercury};
+    background-color: ${({ theme }) => theme.colors.whiteLilac};
     word-break: break-word;
   }
 `;
