ae8e82afc270a26e20fe611fff880f8b6c4b2cb1
GrzegorzJendernal <g.jendernal@gmail.com>
2023-01-25 20:23:04 +0100

    deleted Wrapper

--- src/common/Wrapper/index.js
+++
@@ -1,7 +0,0 @@
-import { Container } from "./styled";
-
-const Wrapper = ({ children }) => (
-    <Container>{children}</Container>
-);
-
-export default Wrapper;
--- src/common/Wrapper/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-    max-width: 1920px;
-    padding-top: 115px;
-    margin: 0px 16px;
-`;
