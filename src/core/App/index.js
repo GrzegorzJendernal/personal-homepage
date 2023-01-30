0a6aa77938266d458324fbf868aa40c18d3b79e5
GrzegorzJendernal <g.jendernal@gmail.com>
2023-01-30 21:56:07 +0100

    added normalized styled

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,19 +1,17 @@
 import { useSelector } from "react-redux";
+import { Normalize } from "styled-normalize";
 import { ThemeProvider } from "styled-components";
 import { selectTheme } from "../../features/PersonalHomepage/homepageSlice";
 import { GlobalStyle } from "./GlobalStyle";
-import { themeDark, themeLight } from "../theme";
+import { themeDark, themeLight } from "./theme";
 import PersonalHomepage from "../../features/PersonalHomepage";
 
 const App = () => {
   const themeName = useSelector(selectTheme);
-  const theme = () => {
-    if (themeName === "dark") return themeDark;
-    return themeLight;
-  };
 
   return (
-    <ThemeProvider theme={theme()}>
+    <ThemeProvider theme={themeName === "dark" ? themeDark : themeLight}>
+      <Normalize />
       <GlobalStyle />
       <PersonalHomepage />
     </ThemeProvider>
