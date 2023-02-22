f048bdee4f7efc7cadc56596d75fb6448fd42f39
GrzegorzJendernal <g.jendernal@gmail.com>
2023-02-22 21:37:06 +0100

    typed Header

--- src/features/PersonalHomepage/Header/ThemeButton/index.tsx
+++ src/features/PersonalHomepage/Header/ThemeButton/index.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectTheme, switchTheme } from "../themeSlice";
+import { selectTheme, switchTheme } from "../../themeSlice";
 import { Button, Circle, Container, Sunny, Text } from "./styled";
 
 const ThemeButton = () => {
