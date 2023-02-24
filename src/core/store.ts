096278c0b5203872a1b9aeafe21382c88762c193
GrzegorzJendernal <g.jendernal@gmail.com>
2023-02-24 21:35:20 +0100

    deleted unused type

--- src/core/store.ts
+++ src/core/store.ts
@@ -15,5 +15,4 @@ const store = configureStore({
 sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
 export default store;
