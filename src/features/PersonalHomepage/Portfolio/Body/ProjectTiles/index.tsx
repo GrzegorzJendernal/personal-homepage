7acc276ff1e4403bf4807d84641b2b57459ccc51
GrzegorzJendernal <g.jendernal@gmail.com>
2023-05-13 20:58:24 +0200

    added condition to render repositories

--- src/features/PersonalHomepage/Portfolio/Body/ProjectTiles/index.tsx
+++ src/features/PersonalHomepage/Portfolio/Body/ProjectTiles/index.tsx
@@ -7,32 +7,35 @@ const ProjectTiles = () => {
   const { data } = useQuery("repos") as Repos;
   return (
     <Container>
-      {data.map((repo) => (
-        <Tile key={repo.id}>
-          <Header>{repo.name}</Header>
-          <Paragraph>{repo.description}</Paragraph>
-          <Paragraph>
-            Demo:&nbsp;
-            <Link
-              href={repo.homepage}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub Pages
-            </Link>
-          </Paragraph>
-          <Paragraph link>
-            Code:&nbsp;
-            <Link
-              href={repo.html_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Repository
-            </Link>
-          </Paragraph>
-        </Tile>
-      ))}
+      {data.map((repo) => {
+        if (!repo.homepage) return;
+        return (
+          <Tile key={repo.id}>
+            <Header>{repo.name}</Header>
+            <Paragraph>{repo.description}</Paragraph>
+            <Paragraph>
+              Demo:&nbsp;
+              <Link
+                href={repo.homepage}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub Pages
+              </Link>
+            </Paragraph>
+            <Paragraph link>
+              Code:&nbsp;
+              <Link
+                href={repo.html_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Repository
+              </Link>
+            </Paragraph>
+          </Tile>
+        );
+      })}
     </Container>
   );
 };
